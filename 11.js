89c3914cf5baf12cf6ebc0548421609606acaa01
Naftali Friedman <fnaftali@amdocs.com>
2018-12-11 22:10:44 +0200

    day 11 part 2

--- 11.js
+++ 11.js
@@ -1,5 +1,3 @@
-const utInput = ``;
-
 const ut1 = ((ipt, grid) => getcellPower(ipt, grid))({ x: 3, y: 5 }, 8);
 assert(ut1 === 4);
 const ut2 = ((ipt, grid) => getcellPower(ipt, grid))({ x: 122, y: 79 }, 57);
@@ -14,43 +12,46 @@ assert(ut4_1 === 29);
 const ut4_2 = (ipt => getGridPower(ipt))([[4, 3, 3], [3, 3, 4], [3, 3, 4]]);
 assert(ut4_2 === 30);
 
-const ut5 = (ipt => getMax3Grid(ipt))(18);
+const ut5 = (ipt => getMaxGrid(ipt))(18);
 assert(ut5.x === 33);
 assert(ut5.y === 45);
-const ut6 = (ipt => getMax3Grid(ipt))(42);
+const ut6 = (ipt => getMaxGrid(ipt))(42);
 assert(ut6.x === 21);
 assert(ut6.y === 61);
 
-function getMax3Grid(grid) {
-  let x,
-    y,
-    maxGrid = 0;
-  const gridSize = 300;
-  const cells = new Array(gridSize);
-  for (let i = 0; i < gridSize; i++) {
-    cells[i] = new Array(gridSize);
-  }
-  for (let i = 0; i < gridSize; i++) {
-    for (let j = 0; j < gridSize; j++) {
-      const power = getcellPower({ x: i, y: j }, grid);
-      cells[i][j] = { power, grid: 0 };
-      if (i > 2 && j > 2) {
-        const prevX = i - 3;
-        const prevY = j - 3;
-        const innerGrid = cells
-          .slice(prevX, i)
-          .map(x => x.slice(prevY, j).map(c => c.power));
 
-        cells[prevX][prevY].grid = getGridPower(innerGrid);
-        if (cells[prevX][prevY].grid > maxGrid) {
-          maxGrid = cells[prevX][prevY].grid;
-          x = prevX;
-          y = prevY;
-        }
-      }
+function getMaxGrid(grid, size = 3, cells = []) {
+  let x, y, maxGrid = -Infinity;
+  const gridSize = 300;
+  if (cells.length === 0) {
+    cells = new Array(gridSize);
+    for (let i = 0; i < gridSize; i++) {
+      cells[i] = new Array(gridSize);
     }
   }
-  return { x, y };
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            if (cells[i][j] === undefined) {
+                const power = getCellPower({ x: i, y: j }, grid);
+                cells[i][j] = { power, grid: 0 };
+            }
+            if (i >= size && j >= size) {
+                const prevX = i - size;
+                const prevY = j - size;
+                const innerGrid = cells
+                    .slice(prevX, i)
+                    .map(x => x.slice(prevY, j).map(c => c.power));
+
+                cells[prevX][prevY].grid = getGridPower(innerGrid);
+                if (cells[prevX][prevY].grid > maxGrid) {
+                    maxGrid = cells[prevX][prevY].grid;
+                    x = prevX;
+                    y = prevY;
+                }
+            }
+        }
+    }
+  return { x, y, cells };
 }
 
 function getGridPower(grid) {
@@ -63,12 +64,59 @@ function getGridPower(grid) {
   return p;
 }
 
-function getcellPower(point, grid) {
+function getCellPower(point, grid) {
   const rackId = point.x + 10;
   let power = Math.floor((rackId * (rackId * point.y + grid)) / 100).toString();
   power = Number(power[power.length - 1]) - 5;
   return power;
 }
 
-let res = getMax3Grid(7857);
-res;
+let { x, y, cells } = getMaxGrid(7857);
+console.log(x,y);
+
+/*****
+ * part 2
+ */
+const ut7 = (ipt => getMaxGrid(ipt, 16))(18);
+assert(ut7.x === 90);
+assert(ut7.y === 269);
+
+const ut8 = (ipt => getMaxGrid(ipt, 12))(42);
+assert(ut8.x === 232);
+assert(ut8.y === 251);
+
+const ut9 = (ipt => getRealMaxGrid(ipt,17))(18);
+assert(ut9.x === 90);
+assert(ut9.y === 269);
+assert(ut9.size === 16);
+const ut10 = (ipt => getRealMaxGrid(ipt,17))(42);
+assert(ut10.x === 232);
+assert(ut10.y === 251);
+assert(ut10.size === 12);
+
+function getRealMaxGrid(grid, totalSize = 300) {
+  let x,
+    y,
+    cx,
+    cy,
+    size,
+    max = 0;
+  let cells = [];
+  for (let i = 1; i <= totalSize; i++) {
+    let mg = getMaxGrid(grid, i, cells);
+    cx = mg.x;
+    cy = mg.y;
+    cells = mg.cells;
+    let lastMaxGrid = cells[cx][cy].grid;
+    if (lastMaxGrid > max) {
+      max = cells[cx][cy].grid;
+      x = cx;
+      y = cy;
+      size = i;
+    }
+    console.log(x, y, size);
+  }
+  return { x, y, size };
+}
+let res2 = getRealMaxGrid(7857);
+console.log(res2);
